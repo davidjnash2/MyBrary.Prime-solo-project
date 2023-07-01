6837e445a68e87cfb70b3afbd87512888f02fdcb
Dave Nash <davidjnash2@gmail.com>
2023-07-01 15:51:05 -0500

    modified GET route on databaserouter to include more id info, since will need that for PUT route; working to get all inputs correctly established to only capture correct data, attempted to add slider selectors for boolean inputs, and brought over some base styling for LibraryBookcss

--- server/routes/database.router.js
+++ server/routes/database.router.js
@@ -14,9 +14,11 @@ router.get('/', (req, res) => {
       "user_book"."rating",
       "user_book"."review",
       "user_book"."borrowed",
-      "user_book"."borrower",
       "user_book"."borrowed_date",
+      "user_book"."borrower",
       "user_book"."time_added",
+      "user_book"."book_id",
+      "book"."id",
       "book"."cover_url",
       "book"."title",
       "book"."subtitle",
@@ -53,7 +55,7 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   if (req.isAuthenticated()) {
     console.log('IN SERVER POST, AND req.body is:', req.body);
-    const book = req.body; // will probably need to define properties
+    const book = req.body; 
     const postBookQuery = `INSERT INTO book (
       cover_url, 
       title,
@@ -124,6 +126,62 @@ router.delete('/:id', (req, res) => {
     res.sendStatus(403)
   }
 });
+
+
+router.put('/:id', (req, res) => {
+  if (req.isAuthenticated()) {
+    console.log('IN SERVER PUT, AND req.body is:', req.body);
+    const book = req.body; // will probably need to define properties
+    const postBookQuery = `INSERT INTO book (
+      cover_url, 
+      title,
+      subtitle,
+      author,
+      publisher,
+      published,
+      genre,
+      pages,	
+      description,
+      isbn)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+    RETURNING "id";
+  `;
+    pool.query(postBookQuery, [
+      book.cover_url,
+      book.title,
+      book.subtitle,
+      book.author,
+      book.publisher,
+      book.published,
+      book.genre,
+      book.pages,
+      book.description,
+      book.isbn]) // book properties from req.body
+      .then((bookResult) => {
+        const newBookId = bookResult.rows[0].id;
+        const userBookQuery = `
+        INSERT INTO "user_book" (user_id, book_id)
+        VALUES ($1, $2);
+        `;
+        pool.query(userBookQuery, [req.user.id, newBookId])
+          .then((response) => {
+            res.sendStatus(202);
+          })
+          .catch((error) => {
+            console.log('ERROR LINKING USER BOOK', error);
+            res.sendStatus(500);
+          });
+      })
+      .catch((error) => {
+        console.log('ERROR IN SERVER POST', error);
+      });
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+
+
 
 
 module.exports = router;
--- src/components/LibraryBook/LibraryBook.jsx
+++ src/components/LibraryBook/LibraryBook.jsx
@@ -1,11 +1,26 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import './LibraryBook.css';
 
 function LibraryBook({ userBook }) {
 
     const [updatedUserBook, setUpdatedUserBook] = useState(userBook);
     const [editing, setEditing] = useState(false);
+
+    const [subtitle, setSubtitle] = useState();
+    const [publisher, setPublisher] = useState();
+    const [published, setPublished] = useState();
+    const [genre, setGenre] = useState();
+    const [pages, setPages] = useState();
+    const [description, setDescription] = useState();
+    const [read, setRead] = useState();
+    const [rating, setRating] = useState();
+    const [review, setReview] = useState();
+    const [borrowed, setBorrowed] = useState();
+    const [borrower, setBorrower] = useState();
+    const [borrowedDate, setBorrowedDate] = useState();
+
 
     const dispatch = useDispatch();
 
@@ -31,7 +46,7 @@ function LibraryBook({ userBook }) {
         dispatch({
             type: 'UPDATE_USER_BOOK',
             payload: {
-                id: userBook.id,
+                id: userBook.book_id,
                 read_status: userBook.read_status,
                 rating: userBook.rating,
                 review: userBook.review,
@@ -41,6 +56,23 @@ function LibraryBook({ userBook }) {
             }
         });
         switchEditing();
+    }
+
+
+    function renderYearOptions() {
+        const currentYear = new Date().getFullYear();
+        const startYear = currentYear;
+        const endYear = currentYear - 999;
+    
+        const options = [];
+        for (let year = startYear; year >= endYear; year--) {
+            options.push(
+                <option key={year} value={year}>
+                    {year}
+                </option>
+            );
+        }
+        return options;
     }
 
 
@@ -54,61 +86,166 @@ function LibraryBook({ userBook }) {
                             <img src={userBook.cover_url} />
                             <p>Title: {userBook.title}</p>
 
+                            <label htmlFor="subtitle">Subtitle:</label>
                             <input
-                                placeholder="Subtitle"
+                                onChange={(event) => setSubtitle(event.target.value)}
+                                type='text'
+                                value={subtitle}
+                                placeholder={userBook.subtitle}
                             />
 
                             <p>Author: {userBook.author}</p>
 
+
+                            <label htmlFor="publisher">Publisher:</label>
                             <input
-                                placholder="Publisher"
+                                onChange={(event) => setPublisher(event.target.value)}
+                                type='text'
+                                value={publisher}
+                                placeholder={userBook.publisher}
                             />
 
+                            <label htmlFor="published">Published:</label>
+                            <select onChange={(event) => setPublished(event.target.value)} value={published}>
+                                {renderYearOptions()}
+                            </select>
+
+
+
+                            <label htmlFor="genre">Genre:</label>
                             <input
-                                placeholder="Published"
+                                onChange={(event) => setGenre(event.target.value)}
+                                type='text'
+                                value={genre}
+                                placeholder={userBook.genre}
                             />
 
-                            <input
-                                placeholder="Genre"
-                            />
 
+                            <label htmlFor="pages">Pages:</label>
                             <input
-                                placeholder="Pages"
+                                onChange={(event) => setPages(event.target.value)}
+                                type='number'
+                                value={pages}
+                                placeholder={userBook.pages}
                             />
 
                             <p>ISBN: {userBook.isbn}</p>
 
-                            <input placeholder={userBook.description} />
+                            <label htmlFor="description">Description:</label>
+                            <input
+                                onChange={(event) => setDescription(event.target.value)}
+                                type='text'
+                                value={description}
+                                placeholder={userBook.description}
+                            />
 
                             <h2>Your info</h2>
 
-                            {/* yes/no toggle box here */}
-                            <p>Read it? {userBook.read_status}</p>
+                            {/* yes/no toggle box here
+                            <label className="switch">
+                                <input
+                                    onChange={(event) => setRead(event.target.checked)}
+                                    type="checkbox"
+                                    value={read} />
+                                <span className="slider round"></span>
+                            </label>
+                            <p>Read it? {userBook.read_status}</p> */}
 
+
+                            <label htmlFor="read">Read?</label>
                             <input
+                                onChange={(event) => setRead(event.target.value)}
+                                type='text'
+                                value={read}
+                                placholder={userBook.read_status.toString()}
+                            />
+
+
+                            {/* need to add slider or stars for rating */}
+                            <label htmlFor="rating">Rating:</label>
+                            {/* <input
+                                onChange={(event) => setComments(event.target.value)}
+                                type='text'
+                                value={description}
+                                placeholder={userBook.rating}
+                            /> */}
+                            <input
+                                onChange={(event) => setRating(event.target.value)}
+                                type='number'
+                                min='0'
+                                max='5'
+                                value={rating}
                                 placeholder={userBook.rating}
                             />
 
+
+
+
+
+
+
+
+
+                            <label htmlFor="review">Review:</label>
                             <input
+                                onChange={(event) => setReview(event.target.value)}
+                                type='text'
+                                value={review}
                                 placholder={userBook.review}
                             />
 
 
                             {/* yes/no toggle box here, too
                                 and if yes, then render the borrowed data fields below */}
-                            
-                            Borrowed?
-                            <input
-                    
-                            />
 
+
+                            <label htmlFor="borrowed">Borrowed:</label>
                             <input
+                                onChange={(event) => setBorrowed(event.target.value)}
+                                type='text'
+                                value={borrowed}
+                                placeholder={userBook.borrowed.toString()}
+                            />
+                            {/* <label>
+                                <input
+                                    onChange={(event) => setBorrowed(event.target.checked)}
+                                    className="switch"
+                                    type="checkbox"
+                                    value={borrowed} />
+                                <span class="slider round"></span>
+                            </label> */}
+
+
+
+
+
+
+
+                            <label htmlFor="borrower">Borrower:</label>
+                            <input
+                                onChange={(event) => setBorrower(event.target.value)}
+                                type='text'
+                                value={borrower}
                                 placeholder={userBook.borrower}
                             />
 
+
+                            <label htmlFor="borrowed_date">Borrowed date:</label>
                             <input
-                                placeholder={userBook.borrowed_date}
+                                type="date"
+                                id="borrowed_date"
+                                value={borrowedDate}
+                                onChange={(event) => setBorrowedDate(event.target.value)}
                             />
+
+                            {/* <label htmlFor="borrowed_date">Borrowed date:</label>
+                            use MUI datepicker here
+                            <input
+                                onChange={(event) => setBorrowedDate(event.target.value)}
+                                type='text'
+                                value={borrowedDate}
+                                placeholder={userBook.borrowed_date}
+                            /> */}
 
                             <button
                                 type="submit"

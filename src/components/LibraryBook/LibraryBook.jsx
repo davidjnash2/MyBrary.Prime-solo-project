c6fbd920ab42d2bcda471147204bd888a80d538c
Dave Nash <davidjnash2@gmail.com>
2023-06-30 23:41:48 -0500

    in LibraryBook component, edit button is correcltly toggling between displaying stored values from db and displaying inputs to edit some of the user data, and book data

--- src/components/LibraryBook/LibraryBook.jsx
+++ src/components/LibraryBook/LibraryBook.jsx
@@ -10,7 +10,7 @@ function LibraryBook({ userBook }) {
     const dispatch = useDispatch();
 
     const switchEditing = () => {
-        setEditing(true);
+        setEditing(!editing);
     }
 
 
@@ -40,7 +40,7 @@ function LibraryBook({ userBook }) {
                 borrower: userBook.borrower,
             }
         });
-        setEditing(false);
+        switchEditing();
     }
 
 
@@ -49,88 +49,162 @@ function LibraryBook({ userBook }) {
             {editing ? (
                 <>
                     <div>
-                        <button>SAVE CHANGES</button>
-                    </div>
-                </>
-            ) : (
-                <>
-                    {userBook && (
-                        <div>
+                        <form onSubmit={updateUserBook}>
                             <h2>Book Info</h2>
-
                             <img src={userBook.cover_url} />
-
                             <p>Title: {userBook.title}</p>
 
-                            {(userBook.subtitle === undefined || userBook.subtitle === null || userBook.subtitle === 0) ? (
-                                <p>Subtitle: n/a</p>
-                            ) : (
-                                <p>Subtitle: {userBook.subtitle}</p>
-                            )}
+                            <input
+                                placeholder="Subtitle"
+                            />
 
                             <p>Author: {userBook.author}</p>
 
-                            {(userBook.publisher === undefined || userBook.publisher === null || userBook.publisher === 0) ? (
-                                <p>Publisher: n/a</p>
-                            ) : (
-                                <p>Publisher: {userBook.publisher}</p>
-                            )}
+                            <input
+                                placholder="Publisher"
+                            />
 
-                            {(userBook.published === undefined || userBook.published === null || userBook.published === 0) ? (
-                                <p>Published: n/a</p>
-                            ) : (
-                                <p>Published: {userBook.published}</p>
-                            )}
+                            <input
+                                placeholder="Published"
+                            />
 
-                            {(userBook.genre === undefined || userBook.genre === null || userBook.genre === 0) ? (
-                                <p>Genre: n/a</p>
-                            ) : (
-                                <p>Genre: {userBook.genre}</p>
-                            )}
+                            <input
+                                placeholder="Genre"
+                            />
 
-                            {(userBook.pages === undefined || userBook.pages === null || userBook.pages === 0) ? (
-                                <p>Pages: n/a</p>
-                            ) : (
-                                <p>Pages: {userBook.pages}</p>
-                            )}
+                            <input
+                                placeholder="Pages"
+                            />
 
                             <p>ISBN: {userBook.isbn}</p>
 
-                            {(userBook.description === undefined || userBook.description === null || userBook.description === 0) ? (
-                                <p>Description: n/a</p>
-                            ) : (
-                                <p>Description: {userBook.description}</p>
-                            )}
+                            <input placeholder={userBook.description} />
 
                             <h2>Your info</h2>
 
                             {/* yes/no toggle box here */}
                             <p>Read it? {userBook.read_status}</p>
 
-                            {(userBook.rating === undefined || userBook.rating === null || userBook.rating === 0) ? (
-                                <p>Like it?  n/a</p>
-                            ) : (
-                                <p>Like it? {userBook.rating}</p>
-                            )}
+                            <input
+                                placeholder={userBook.rating}
+                            />
 
-                            {(userBook.review === undefined || userBook.review === null || userBook.review === 0) ? (
-                                <p>What'd you think? n/a</p>
-                            ) : (
-                                <p>What'd you think? {userBook.review}</p>
-                            )}
-                            
+                            <input
+                                placholder={userBook.review}
+                            />
+
+
                             {/* yes/no toggle box here, too
                                 and if yes, then render the borrowed data fields below */}
-                            <p>Somebody got it currently? {userBook.borrowed}</p>
+                            
+                            Borrowed?
+                            <input
+                    
+                            />
 
-                            <p>Who? {userBook.borrower}</p>
-                            <p>Since when? {userBook.borrowed_date}</p>
+                            <input
+                                placeholder={userBook.borrower}
+                            />
+
+                            <input
+                                placeholder={userBook.borrowed_date}
+                            />
+
+                            <button
+                                type="submit"
+                            >
+                                SAVE CHANGES
+                            </button>
+                        </form>
+                    </div>
+                </>
+            ) : (
+                <>
+                    <div>
+                        <h2>Book Info</h2>
+
+                        <img src={userBook.cover_url} />
+
+                        <p>Title: {userBook.title}</p>
+
+                        {(userBook.subtitle === undefined || userBook.subtitle === null || userBook.subtitle === 0) ? (
+                            <p>Subtitle: n/a</p>
+                        ) : (
+                            <p>Subtitle: {userBook.subtitle}</p>
+                        )}
+
+                        <p>Author: {userBook.author}</p>
+
+                        {(userBook.publisher === undefined || userBook.publisher === null || userBook.publisher === 0) ? (
+                            <p>Publisher: n/a</p>
+                        ) : (
+                            <p>Publisher: {userBook.publisher}</p>
+                        )}
+
+                        {(userBook.published === undefined || userBook.published === null || userBook.published === 0) ? (
+                            <p>Published: n/a</p>
+                        ) : (
+                            <p>Published: {userBook.published}</p>
+                        )}
+
+                        {(userBook.genre === undefined || userBook.genre === null || userBook.genre === 0) ? (
+                            <p>Genre: n/a</p>
+                        ) : (
+                            <p>Genre: {userBook.genre}</p>
+                        )}
+
+                        {(userBook.pages === undefined || userBook.pages === null || userBook.pages === 0) ? (
+                            <p>Pages: n/a</p>
+                        ) : (
+                            <p>Pages: {userBook.pages}</p>
+                        )}
+
+                        <p>ISBN: {userBook.isbn}</p>
+
+                        {(userBook.description === undefined || userBook.description === null || userBook.description === 0) ? (
+                            <p>Description: n/a</p>
+                        ) : (
+                            <p>Description: {userBook.description}</p>
+                        )}
+
+                        <h2>Your info</h2>
+
+                        {/* yes/no toggle box here */}
+                        <p>Read it? {userBook.read_status}</p>
+
+                        {(userBook.rating === undefined || userBook.rating === null || userBook.rating === 0) ? (
+                            <p>Like it?  n/a</p>
+                        ) : (
+                            <p>Like it? {userBook.rating}</p>
+                        )}
+
+                        {(userBook.review === undefined || userBook.review === null || userBook.review === 0) ? (
+                            <p>What'd you think? n/a</p>
+                        ) : (
+                            <p>What'd you think? {userBook.review}</p>
+                        )}
+
+                        {/* yes/no toggle box here, too
+                                and if yes, then render the borrowed data fields below */}
+                        <p>Somebody got it currently? {userBook.borrowed}</p>
+
+                        <p>Who? {userBook.borrower}</p>
+                        <p>Since when? {userBook.borrowed_date}</p>
 
 
-                            <button onClick={switchEditing}>EDIT</button>
-                            <button name="delete" onClick={deleteUserBook}>DELETE BOOK</button>
-                        </div>
-                    )}
+                        <button
+                            type="button"
+                            onClick={switchEditing}
+                        >
+                            EDIT
+                        </button>
+                        <button
+                            name="delete"
+                            onClick={deleteUserBook}
+                        >
+                            DELETE BOOK
+                        </button>
+                    </div>
                 </>
             )}
         </>
